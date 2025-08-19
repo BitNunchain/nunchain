6f56f5f5037a02b94a0b8e088ea583a134fd8bc6
BitNun Blockchain <info@unifiednun.com>
2025-08-19 04:35:12 +0200

    Create api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,8 @@
+/**
+ * Simple GET wrapper
+ */
+export async function apiGet<T>(url: string): Promise<T> {
+  const res = await fetch(url)
+  if (!res.ok) throw new Error(`API error: ${res.status}`)
+  return res.json()
+}
