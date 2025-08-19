9555c3edff11463737e80ff522143941b50a5688
BitNun Blockchain <info@unifiednun.com>
2025-08-19 04:42:28 +0200

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 text-center text-xs text-gray-500 border-t border-gray-800">
+      © {new Date().getFullYear()} Nunchain
+    </footer>
+  )
+}
